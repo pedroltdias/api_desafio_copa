ef493b988470eb3c222bd4c6e24a50cf7fa4e940
pedroltdias <pedroltdias@gmail.com>
2022-12-19 10:12:28 -0300

    controllers e CRUD de Teams, Match, MatchTeam construidos

---
+++ app/controllers/api/v1/match_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::MatchController < ApplicationController
+    before_action :set_match, only: [:show, :update, :destroy]
+
+    def index
+        @matches = Match.all
+
+        render json: @matches
+    end
+
+    def show
+        render json: @match
+    end
+
+    def create
+        @match = Match.new(match_params)
+
+        if @match.save  
+            render json: @match, status: :created_at
+        else
+            render json: @match.erros, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @match.update(match_params)
+            render json: @match
+        else
+            render json: @match.erros, status: :unprocessable_entity
+        end        
+    end
+
+    def destroy
+        @match.destroy
+    end
+
+    private
+    def set_match
+        @match = match.find(params[:id])
+    end
+
+    def match_params
+        params.require(:match).permit(:name)
+    end
+end
---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::TeamsController < ApplicationController
+    before_action :set_team, only: [:show, :update, :destroy]
+
+    def index
+        @teams = Team.all
+
+        render json: @teams
+    end
+
+    def show
+        render json: @team
+    end
+
+    def create
+        @team = Team.new(team_params)
+
+        if @team.save  
+            render json: @team, status: :created_at
+        else
+            render json: @team.erros, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        if @team.update(team_params)
+            render json: @team
+        else
+            render json: @team.erros, status: :unprocessable_entity
+        end        
+    end
+
+    def destroy
+        @team.destroy
+    end
+
+    private
+    def set_team
+        @team = Team.find(params[:id])
+    end
+
+    def team_params
+        params.require(:team).permit(:name)
+    end
+end
